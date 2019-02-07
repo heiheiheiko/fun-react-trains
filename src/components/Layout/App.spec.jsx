abc7b7a337647c5d6dafbc13b12ffeb82923edb1
Heiko Beineke <heiko.beineke@gmail.com>
2019-02-07 23:46:55 +0100

    Remove unnecessary Redux mocking overhead from specs without Redux

--- src/components/Layout/App.spec.jsx
+++ src/components/Layout/App.spec.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import configureStore from 'redux-mock-store';
-import { Provider } from "react-redux";
 import renderer from 'react-test-renderer'
 
 import App from './App';
@@ -9,21 +7,12 @@ import App from './App';
 jest.mock('./AppAside', () => ()=> <div> AppAside </div>);
 
 describe('<App>', () => {
-  const initialState = { mapCenter: [1,1] };
-  const mockStore = configureStore();
-  let store, componentContainer;
-
-  beforeEach(() => {
-    store = mockStore(initialState)
-    componentContainer = <Provider store={store}><App /></Provider>;
-  })
-
   it('could be renderd', () => {
-    expect(mount(componentContainer).find(App).length).toEqual(1)
+    expect(mount(<App/>).find(App).length).toEqual(1)
   });
 
   it('matched the snapshot', () => {
-    const renderedValue =  renderer.create(componentContainer).toJSON()
+    const renderedValue =  renderer.create(<App/>).toJSON()
     expect(renderedValue).toMatchSnapshot();
   });
 });
--- src/components/Layout/AppAside.spec.jsx
+++ src/components/Layout/AppAside.spec.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { mount } from 'enzyme';
-import configureStore from 'redux-mock-store';
-import { Provider } from "react-redux";
 import renderer from 'react-test-renderer'
 
 import AppAside from './AppAside';
@@ -11,21 +9,12 @@ jest.mock('./MapZoomControls', () => ()=> <div> MapZoomControls </div>);
 jest.mock('./MapCenterLabel', () => ()=> <div> MapCenterLabel </div>);
 
 describe('<AppAside>', () => {
-  const initialState = { mapCenter: [1,1] };
-  const mockStore = configureStore();
-  let store, componentContainer;
-
-  beforeEach(() => {
-    store = mockStore(initialState)
-    componentContainer = <Provider store={store}><AppAside /></Provider>;
-  })
-
   it('could be renderd', () => {
-    expect(mount(componentContainer).find(AppAside).length).toEqual(1)
+    expect(mount(<AppAside/>).find(AppAside).length).toEqual(1)
   });
 
   it('matched the snapshot', () => {
-    const renderedValue =  renderer.create(componentContainer).toJSON()
+    const renderedValue =  renderer.create(<AppAside/>).toJSON()
     expect(renderedValue).toMatchSnapshot();
   });
 });
